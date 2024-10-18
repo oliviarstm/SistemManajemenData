8ec819cbb083f7757a497bfd1e0006e1c05d97ff
klnkvc <klnkvc@gmail.com>
2024-10-18 22:46:05 +0700

    data kelas mentee fungsional

--- src/Pages/mentee/DataKelas.jsx
+++ src/Pages/mentee/DataKelas.jsx
@@ -1,4 +1,7 @@
 import Table from "../../Components/table/Index.jsx";
+import axios from "../../utils/axios.js";
+import {useEffect, useState} from "react";
+import {useSelector} from "react-redux";
 
 const exTitle = "Data Kelas";
 const exField = ["Nama", "Universitas", "Kelas", "Sesi"];
@@ -16,10 +19,30 @@ const exData = [{
     Session:"Siang"
 }]
 const DataKelas=()=>{
+    const [menteeData, setMenteeData] = useState([]);
+    const {accountId} = useSelector(state => state.Auth)
+
+    const fetchData =async ()=>{
+        try {
+            const getKelas =await axios.get(`/mentee/${accountId}`)
+            const kelas = getKelas.data.data.class
+            let result;
+            const res = await axios.get(`/menteeclass/?class=${kelas}`);
+            result = res.data.data;
+            setMenteeData(result); // Set menteeData
+        } catch (error) {
+            console.error("Error fetching mentee data:", error); // Log any errors
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, []);
+
     const propsData={
         title:exTitle,
         field:exField,
-        data:exData,
+        data:menteeData,
         isEnable:false,
         tableType:"none"
     }
