ec1b36bb9d64748df965a23eb754c300fa4baa6e
klnkvc <klnkvc@gmail.com>
2024-11-03 21:09:30 +0700

    Change logo IL imports

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import ButtonLogin from "./ButtonLogin";
+import LogoIL from "../../public/logo-il.png"
 
 const Navbar = () => {
   return (
     <>
       <div className="navbar shadow drop-shadow-lg px-10">
         <div className="flex-1">
-          <img src="./logo-il.png" className="w-28 h-14" alt="" />
+          <img src={LogoIL} className="w-28 h-14" alt="" />
         </div>
         <div className="flex gap-5">
           <button>Beranda</button>
